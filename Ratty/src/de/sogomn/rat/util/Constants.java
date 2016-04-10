c7016adf8526c53984f1ddb8c03ad03b56822c60
Sogomn <Sogomn@Sogomn-PC>
2016-04-10 22:37:09 +0200

    Minor changes Translations Changed version tag

--- Ratty/src/de/sogomn/rat/util/Constants.java
+++ Ratty/src/de/sogomn/rat/util/Constants.java
@@ -17,7 +17,7 @@ public final class Constants {
 	
 	private static final String DATA_PATH = "/data";
 	
-	public static final String VERSION = "1.24.0";
+	public static final String VERSION = "1.25.0";
 	public static final ResourceBundle LANGUAGE = ResourceBundle.getBundle("language.lang");
 	public static final IOperatingSystemService OS_SERVICE = IOperatingSystemService.getInstance();
 	public static final String ADDRESS;
